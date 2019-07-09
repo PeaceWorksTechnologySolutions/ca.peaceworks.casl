5b2d157ac40d5e7666804e9d67ed295a7c57cbc7
martinh-pw <martinh@peaceworks.ca>
2019-07-09 16:18:03 +0200

    Initial dump of code from previous repo (doesn't include install code)

--- casl.php
+++ casl.php
@@ -168,3 +168,109 @@ function casl_civicrm_navigationMenu(&$menu) {
   ));
   _casl_civix_navigationMenu($menu);
 } // */
+
+function _casl_get_casl_group_id() {
+    $result = civicrm_api3('CustomGroup', 'get', [
+      'sequential' => 1,
+      'name' => 'casl',
+    ]);
+    return $result['id'];
+}
+
+function _casl_get_consent_type_id() {
+    $result = civicrm_api3('CustomField', 'get', [
+      'sequential' => 1,
+      'name' => "consent_type",
+    ]);
+    return 'custom_' . $result['id'];
+}
+
+function _casl_get_consent_date_id() {
+    $result = civicrm_api3('CustomField', 'get', [
+      'sequential' => 1,
+      'name' => "consent_date",
+    ]);
+    return 'custom_' . $result['id'];
+}
+
+function _casl_test_expiration($consent_date) {
+    //Return expiration boolean based on whether the expiry time is before or after current time
+    $consent_date = new DateTime($consent_date);
+    if (date_modify($consent_date, '+2 years') < new DateTime()) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+function _casl_set_do_not_email_flag($contact_ID, $do_not_email_flag) {
+   $params = array (
+        'id' => $contact_ID,
+        'do_not_email' => $do_not_email_flag
+   );
+   $result = civicrm_api3('contact', 'create', $params);
+}
+
+/**
+When a contact's custom fields are updated, uses CASL fields to determine the "Do Not Email" flag
+**/
+function casl_civicrm_custom($op,$groupid,$entityid,&$params) {
+
+    $consent_group_id = _casl_get_casl_group_id();
+    $consent_type_id = _casl_get_consent_type_id();
+    $consent_date_id = _casl_get_consent_date_id();
+
+    if (($groupid==$consent_group_id) and ($op=='create' || $op=='edit')) {
+        civicrm_initialize();
+        $result = civicrm_api3('Contact', 'get', array(
+            'contact_id' => $entityid,
+            'return' => "contact_type,". $consent_type_id .",". $consent_date_id,
+        ));
+        $consent_type = $result['values'][$entityid][$consent_type_id];
+        $expired = FALSE;
+
+        //Test if implicit consent has expired after 2 years
+        if ($consent_type == "Implicit") {
+            $consent_date = $result['values'][$entityid][$consent_date_id];
+            $expired = _casl_test_expiration($consent_date);
+        }
+     
+        if (($consent_type == "Implicit" && !$expired) || $consent_type == "Explicit" || $consent_type == "Exempt"){
+            //Two scenarios where CASL allows for mailing lists: implicit consent given within 2 years, or explicit consent anytime
+            //FIXME: leave flag as is!
+            //_casl_set_do_not_email_flag($entityid,FALSE);
+        }
+        else {
+            //Any other scenario, including no consent given and implicit more than 2 years old
+            _casl_set_do_not_email_flag($entityid,TRUE);
+        }
+    }
+}
+
+/**
+Searches all contacts on cron to find any instances of implicit consent having expired
+Turns on the Do Not Email flag if consent has expired
+FIXME: check civicrm cron hook documentation
+**/
+function casl_civicrm_cron() {
+    civicrm_initialize();
+
+    $consent_group_id = _casl_get_casl_group_id();
+    $consent_type_id = _casl_get_consent_type_id();
+    $consent_date_id = _casl_get_consent_date_id();
+
+    $result = civicrm_api3('Contact', 'get', array(
+        $consent_type_id => "Implicit",
+        'do_not_email' => 0,
+        'options' => array('limit' => 0),
+        'return' => "contact_type,". $consent_type_id .",". $consent_date_id,
+    ));
+
+    foreach ($result['values'] as $contact) {
+        $consent_date = $contact[$consent_date_id];
+        $contact_ID = $contact['id']; 
+        if (_casl_test_expiration($consent_date)) {
+            _casl_set_do_not_email_flag($contact_ID,TRUE);
+        }
+    }
+}
+
